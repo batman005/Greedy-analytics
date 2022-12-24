6f8111247eb51d4570700ea8fa19f4bd9bdd0b97
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2022-12-24 17:06:19 +0530

    api

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -34,13 +34,13 @@ const DatePicker = ({ isVisible, onClose }) => {
                         <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
                             <form onSubmit={handleSubmit}>
                                 <div className="form-group mb-6">
-                                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                    <label className="block text-gray-700 text-sm font-bold mb-2" >
                                         StartDate
                                     </label>
                                     <input type="text" className="form-control block w-full  px-3    py-1.5  text-base  font-normal  text-gray-700  bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
                                 </div>
                                 <div className="form-group mb-6">
-                                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
                                         EndDate
                                     </label>
                                     <input type="text" className="form-control block w-full   px-3   py-1.5 text-base font-normal    text-gray-700   bg-white bg-clip-padding  border border-solid border-gray-300 rounded transition ease-in-out   m-0    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
